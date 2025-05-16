1e679fd6bb7a9483cc4d7b3d02b815722e1c2ee7
Adrian Leon <leon.adriand@gmail.com>
2025-05-16 11:22:36 -0600

    Add convertMinutestosecond

--- edabit/day1/ConvertMinutesToSeconds.java
+++ edabit/day1/ConvertMinutesToSeconds.java
@@ -1,5 +1,17 @@
 public class ConvertMinutesToSeconds {
     public static void main(String[] args){
-        System.out.println("hello world");
+        try{
+            int minutes = -30;
+            System.out.println("The minutes to seconds is: " + convertMinutesToSeconds(minutes));
+        }catch(Exception exception){
+            System.err.println("Error: " + exception.getMessage());
+        }
+    }
+
+    public static int convertMinutesToSeconds(int minutes) throws Exception{
+        if (minutes < 0){
+            throw new IllegalArgumentException("Negative numbers not allowed");
+        }
+        return minutes * 60;
     }
 }
--- edabit/day1/reto1_sum.java
+++ edabit/day1/reto1_sum.java
@@ -1,18 +1,18 @@
 public class reto1_sum{
-    public static void main(String[] args){
-        try{
-            int value_one = -3;
-            int value_two = 6;
-            int value_three = 5;
-            int value_four = 5;
-            int sum_one = sumTwoNumber(value_one, value_two);
-            int sum_two = sumTwoNumber(value_three, value_four);
-            System.out.println("Sum of " + value_one + " + " + value_two + " = " + sum_one);
-            System.out.println("Sum of " + value_three + " + " + value_four + " = " + sum_two);
-        } catch(Exception e){
-            System.err.println("Error: " + e.getMessage());
-        }
-    }
+    // public static void main(String[] args){
+    //     try{
+    //         int value_one = -3;
+    //         int value_two = 6;
+    //         int value_three = 5;
+    //         int value_four = 5;
+    //         int sum_one = sumTwoNumber(value_one, value_two);
+    //         int sum_two = sumTwoNumber(value_three, value_four);
+    //         System.out.println("Sum of " + value_one + " + " + value_two + " = " + sum_one);
+    //         System.out.println("Sum of " + value_three + " + " + value_four + " = " + sum_two);
+    //     } catch(Exception e){
+    //         System.err.println("Error: " + e.getMessage());
+    //     }
+    // }
 
     public static int sumTwoNumber(int number_one, int number_two) throws Exception{
         if (number_one < 0 || number_two < 0) { 
